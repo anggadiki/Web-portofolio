a617ce8e81d14229bd697b4b36494c0a06baa3a7
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-11-29 13:08:19 +0700

    add folder routes artikel,blog and create atomic design components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-export default function Home() {
-  return (
-    <div className="">
-      <p className=" font-ubuntu">Hello word</p>
-      <p className=" font-ibm"> Test font</p>
-    </div>
-  );
-}
+import HomePage from "@/components/pages/HomePage";
+
+const Home = () => {
+  return <HomePage />;
+};
+
+export default Home;
---
+++ src/components/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import Nav from "@/components/moleculs/Nav";
+
+const HomePage = () => {
+  return (
+    <div className=" w-full">
+      <Nav />
+    </div>
+  );
+};
+
+export default HomePage;
