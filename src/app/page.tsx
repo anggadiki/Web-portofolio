66801110302bf2b8b77701f57f64670566441b45
root <anggadikisaputraagung@gmail.com>
2023-11-29 12:05:52 +0700

    set up folder next app and font family

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="">
+      <p className=" font-ubuntu">Hello word</p>
+      <p className=" font-ibm"> Test font</p>
+    </div>
+  );
+}
