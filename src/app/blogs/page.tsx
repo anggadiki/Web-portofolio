fbee815c22544684c2b87a2d501af8796bf9b048
Angga diki Saputra <anggadikisaputraagung@gmail.com>
2023-12-09 18:56:39 +0700

    create blogs page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,7 @@
+import BlosgPage from "@/components/pages/BlogPage";
+
+const Blogs = () => {
+  return <BlosgPage />;
+};
+
+export default Blogs;
---
+++ src/components/pages/BlogPage/index.tsx
@@ -0,0 +1,13 @@
+import Nav from "@/components/molecules/Nav";
+
+const BlosgPage = () => {
+  return (
+    <>
+      <div className=" w-full">
+        <Nav />
+      </div>
+    </>
+  );
+};
+
+export default BlosgPage;
